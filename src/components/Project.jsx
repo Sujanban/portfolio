484b1c1ac7cc3fb93aa5f9db4aeaa2d3afc009c7
Sujan Ban <bansujan@gmail.com>
2024-04-08 09:05:45 +0545

    project added

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -4,6 +4,7 @@ import { FiGithub } from "react-icons/fi";
 import { BiLinkExternal } from "react-icons/bi";
 import project1 from "../images/project1.webp";
 import project2 from "../images/project2.webp";
+import project3 from "../images/project3.png";
 
 export const Project = () => {
     return (
@@ -15,17 +16,17 @@ export const Project = () => {
                     </li>
                     <div className='relative py-4 md:py-8 flex items-center my-12'>
                         <div className='w-[60%] bg-[#1C4753] hidden md:block'>
-                            <img className=' transition duration-200 ease-in  opacity-[0.2] hover:opacity-100 w-full h-[100%] rounded' src={project1} alt="realestate marketplace developed by sujan ban" />
+                            <img className=' transition duration-200 ease-in  opacity-[0.2] hover:opacity-100 w-full h-[100%] rounded' src={project3} alt="realestate marketplace developed by sujan ban" />
                         </div>
                         <div className='w-full lg:max-w-[50%] md:max-w-[80%] md:text-end md:absolute bg-bgcard md:bg-transparent md:right-0'>
                             <div className=''>
                                 <h3 className='p-4 md:px-0 md:py-4 text-neon text-sm md:text-base'>Featured Project</h3>
-                                <a className='p-4 md:px-0 md:py-4 text-xl md:text-2xl font-semibold' href="">Realestate Marketplace</a>
-                                <div className='p-4 text-colorsecondary rounded bg-bgcard text-sm'>A minimalistic marketplace where realestate owners can list their propery and the customer can view and contact to respective owner for buying & selling.</div>
-                                <li className='flex-wrap flex justify-start md:justify-end list-none p-4 md:px-0 md:py-4 text-colorsecondary text-sm'> <span className='p-2'>Javascript</span><span className='p-2'>PHP</span><span className='p-2'>Laravel</span><span className='p-2'>MySql</span> </li>
+                                <h1 className='p-4 md:px-0 md:py-4 text-xl md:text-2xl font-semibold'>Collab - Crowdfunding Platform</h1>
+                                <div className='p-4 text-colorsecondary rounded bg-bgcard text-sm'>Project consist of multiple pages with main focus on frontend with little implemention of backend</div>
+                                <li className='flex-wrap flex justify-start md:justify-end list-none p-4 md:px-0 md:py-4 text-colorsecondary text-sm'><span className='p-2'>React Js</span><span className='p-2'>Node</span><span className='p-2'>Mongo DB</span><span className='p-2'>Express</span><span className='p-2'>Tailwind CSS</span> </li>
                                 <p className='p-4 md:px-0 md:py-4 flex text-end md:float-right'>
                                     <a className='p-2 text-end' href=""><BiLinkExternal className='text-end' /></a>
-                                    <a className='p-2' href="https://github.com/Sujanban/realesto-realestate-marketplace" target="_blank"><FiGithub /></a>
+                                    <a className='p-2' href="https://github.com/Sujanban/crowdfunding_np" target="_blank"><FiGithub /></a>
                                 </p>
                             </div>
                         </div>
@@ -37,13 +38,31 @@ export const Project = () => {
                         <div className='w-full lg:max-w-[50%] md:max-w-[80%] md:text-start md:absolute bg-bgcard md:bg-transparent left-0'>
                             <div className=''>
                                 <h3 className='p-4 md:px-0 md:py-4 text-neon text-sm md:text-base'>Featured Project</h3>
-                                <a className='p-4 md:px-0 md:py-4 text-xl md:text-2xl font-semibold' href="">Shopzilla - Ecommerce Website</a>
-                                <div className='p-4 text-colorsecondary rounded bg-bgcard text-sm'>This project contains a fully functional Ecommerce frontend with react js</div>
+                                <h1 className='p-4 md:px-0 md:py-4 text-xl md:text-2xl font-semibold'>Ecommerce Shop</h1>
+                                <div className='p-4 text-colorsecondary rounded bg-bgcard text-sm'>Project consist of Ecommerce functionality with pages like home, explore, search, cart, checkout.</div>
                                 <li className='flex-wrap flex list-none p-4 md:px-0 md:py-4 text-colorsecondary text-sm'> <span className='p-2'>React Js</span><span className='p-2'>Tailwind CSS</span> </li>
                                 <li className='p-4 md:px-0 md:py-4 list-none flex text-end'>
                                     {/* <a className='p-2' href="https://sujanban.github.io/shortner/"><BiLinkExternal /></a> */}
                                     <a className='p-2' href="https://github.com/Sujanban/Shopzilla_Ecommerce" target='_blank'><FiGithub /></a>
                                     </li>
+                            </div>
+                        </div>
+                    </div>
+                    
+                    <div className='relative py-4 md:py-8 flex items-center my-12'>
+                        <div className='w-[60%] bg-[#1C4753] hidden md:block'>
+                            <img className=' transition duration-200 ease-in  opacity-[0.2] hover:opacity-100 w-full h-[100%] rounded' src={project1} alt="realestate marketplace developed by sujan ban" />
+                        </div>
+                        <div className='w-full lg:max-w-[50%] md:max-w-[80%] md:text-end md:absolute bg-bgcard md:bg-transparent md:right-0'>
+                            <div className=''>
+                                <h3 className='p-4 md:px-0 md:py-4 text-neon text-sm md:text-base'>Featured Project</h3>
+                                <h1 className='p-4 md:px-0 md:py-4 text-xl md:text-2xl font-semibold'>Realestate Marketplace</h1>
+                                <div className='p-4 text-colorsecondary rounded bg-bgcard text-sm'>A minimalistic marketplace where realestate owners can list their propery and the customer can view and contact to respective owner for buying & selling.</div>
+                                <li className='flex-wrap flex justify-start md:justify-end list-none p-4 md:px-0 md:py-4 text-colorsecondary text-sm'> <span className='p-2'>Javascript</span><span className='p-2'>PHP</span><span className='p-2'>Laravel</span><span className='p-2'>MySql</span> </li>
+                                <p className='p-4 md:px-0 md:py-4 flex text-end md:float-right'>
+                                    {/* <a className='p-2 text-end' href=""><BiLinkExternal className='text-end' /></a> */}
+                                    <a className='p-2' href="https://github.com/Sujanban/realesto-realestate-marketplace" target="_blank"><FiGithub /></a>
+                                </p>
                             </div>
                         </div>
                     </div>
@@ -54,12 +73,11 @@ export const Project = () => {
                             <div className='my-2 transtition duration-200 ease-in relative top-0 hover:-top-[10px]  hover:text-colorprimary rounded bg-bgcard shadow-[0_35px_60px_-15px_rgba(0,0,0,0.3)] p-4 md:p-8 text-left' href=''>
                                 <div className='flex justify-between items-center py-2'>
                                     <AiOutlineFolder className='text-5xl text-neon' />
-                                    <a className='hover:text-colorprimary' href=""><FiGithub className='text-2xl text-colorsecondary' /></a>
+                                    <a className='hover:text-colorprimary' target='_blank' href="https://github.com/Sujanban/BlockGallery"><FiGithub  className='text-2xl text-colorsecondary' /></a>
                                 </div>
-                                <h2 className='py-2 font-bold hover:text-neon'>Word and Character Counter</h2>
-                                <p className='text-sm py-2 text-colorsecondary'>Basic word counter webapp developed using Javascript
-                                </p>
-                                <p className='py-2 text-sm text-colorsecondary'><span>HTML</span><span className='p-2'>CSS</span><span>Javascript</span></p>
+                                <h2 className='py-2 font-bold hover:text-neon'>BlockGallery - NFT Marketplace</h2>
+                                <p className='text-sm py-2 text-colorsecondary'>A Web 3 based NFT Marketplace for crypto collectibles.</p>
+                                <p className='py-2 text-sm text-colorsecondary'><span>React Js</span><span className='p-2'>Solidity</span><span>Piniata</span></p>
                             </div>
                             <div className='my-2 transtition duration-200 ease-in relative top-0 hover:-top-[10px] hover:text-colorprimary rounded bg-bgcard shadow-[0_35px_60px_-15px_rgba(0,0,0,0.3)] p-4 md:p-8 text-left' href=''>
                                 <div className='flex justify-between items-center py-2'>
@@ -67,65 +85,23 @@ export const Project = () => {
                                         <AiOutlineFolder className='text-5xl text-neon' />
                                     </div>
                                     <div className='flex items-center justify-center'>
-                                        <a className='hover:text-colorprimary' href=""><FiGithub className='text-2xl m-2 text-colorsecondary' /></a>
-                                        <a href=""><BiLinkExternal className='text-2xl m-2 text-colorsecondary' /></a>
+                                        <a className='hover:text-colorprimary' target='_blank' href="https://github.com/Sujanban/Meraki_collab"><FiGithub className='text-2xl m-2 text-colorsecondary' /></a>
+                                        {/* <a href=""><BiLinkExternal className='text-2xl m-2 text-colorsecondary' /></a> */}
                                     </div>
                                 </div>
-                                <h2 className='py-2 font-bold hover:text-neon'>Basic Shanke Game using Javascript</h2>
-                                <p className='text-sm py-2 text-colorsecondary'>Building a basic Snake game using javascript the concept of javascript canvas
-                                </p>
-                                <p className='py-2 text-sm text-colorsecondary'><span>HTML</span><span className='p-2'>CSS</span><span>Javascript</span></p>
+                                <h2 className='py-2 font-bold hover:text-neon'>Meraki Collab - A charity platform</h2>
+                                <p className='text-sm py-2 text-colorsecondary'>Meraki collab is a charity platform where user can donate and recieve donations.</p>
+                                <p className='py-2 text-sm text-colorsecondary'><span>PHP</span><span className='p-2'>MySql</span></p>
                             </div>
                             <div className='my-2 transtition duration-200 ease-in relative top-0 hover:-top-[10px]  hover:text-colorprimary rounded bg-bgcard shadow-[0_35px_60px_-15px_rgba(0,0,0,0.3)] p-4 md:p-8 text-left' href=''>
                                 <div className='flex justify-between items-center py-2'>
                                     <AiOutlineFolder className='text-5xl text-neon' />
-                                    <a className='hover:text-colorprimary' href=""><FiGithub className='text-2xl text-colorsecondary' /></a>
+                                    <a className='hover:text-colorprimary' target='_blank' href="https://github.com/Sujanban/python_chatbot"><FiGithub className='text-2xl text-colorsecondary' /></a>
                                 </div>
-                                <h2 className='py-2 font-bold hover:text-neon'>Weather App using - Openweathermap API</h2>
-                                <p className='text-sm py-2 text-colorsecondary'>Built a fully functional weather app where user can search the weather of particular location.
-                                </p>
-                                <p className='py-2 text-sm text-colorsecondary'><span>React Js</span><span className='p-2'>Tailwind CSS</span><span></span></p>
+                                <h2 className='py-2 font-bold hover:text-neon'>AgroBot - A Python Chatbot</h2>
+                                <p className='text-sm py-2 text-colorsecondary'>Agrobot is a python based chatbot with the rich dataset on farming.</p>
+                                <p className='py-2 text-sm text-colorsecondary'><span>PHP</span><span className='p-2'>Python</span><span></span></p>
                             </div>
-                            {/* <div className='my-2 transtition duration-200 ease-in relative top-0 hover:-top-[10px] hover:text-colorprimary rounded bg-bgcard shadow-[0_35px_60px_-15px_rgba(0,0,0,0.3)] p-4 md:p-8 text-left' href=''>
-                                <div className='flex justify-between items-center py-2'>
-                                    <div>
-                                        <AiOutlineFolder className='text-5xl text-neon' />
-                                    </div>
-                                    <div className='flex items-center justify-center'>
-                                        <a className='hover:text-colorprimary' href=""><FiGithub className='text-2xl m-2 text-colorsecondary' /></a>
-                                        <a href=""><BiLinkExternal className='text-2xl m-2 text-colorsecondary' /></a>
-                                    </div>
-                                </div>
-                                <h2 className='py-2 font-bold hover:text-neon'>Integrating Algolia Search with WordPress Multisite</h2>
-                                <p className='text-sm py-2 text-colorsecondary'>Building a custom multisite compatible WordPress plugin to build global search with Algolia
-                                </p>
-                                <p className='py-2 text-sm text-colorsecondary'><span>Algolia</span><span className='p-2'>Wordpress</span><span>PHP</span></p>
-                            </div>
-                            <div className='my-2 transtition duration-200 ease-in relative top-0 hover:-top-[10px]  hover:text-colorprimary rounded bg-bgcard shadow-[0_35px_60px_-15px_rgba(0,0,0,0.3)] p-4 md:p-8 text-left' href=''>
-                                <div className='flex justify-between items-center py-2'>
-                                    <AiOutlineFolder className='text-5xl text-neon' />
-                                    <a className='hover:text-colorprimary' href=""><FiGithub className='text-2xl text-colorsecondary' /></a>
-                                </div>
-                                <h2 className='py-2 font-bold hover:text-neon'>Integrating Algolia Search with WordPress Multisite</h2>
-                                <p className='text-sm py-2 text-colorsecondary'>Building a custom multisite compatible WordPress plugin to build global search with Algolia
-                                </p>
-                                <p className='py-2 text-sm text-colorsecondary'><span>Algolia</span><span className='p-2'>Wordpress</span><span>PHP</span></p>
-                            </div>
-                            <div className='my-2 transtition duration-200 ease-in relative top-0 hover:-top-[10px] hover:text-colorprimary rounded bg-bgcard shadow-[0_35px_60px_-15px_rgba(0,0,0,0.3)] p-4 md:p-8 text-left' href=''>
-                                <div className='flex justify-between items-center py-2'>
-                                    <div>
-                                        <AiOutlineFolder className='text-5xl text-neon' />
-                                    </div>
-                                    <div className='flex items-center justify-center'>
-                                        <a className='hover:text-colorprimary' href=""><FiGithub className='text-2xl m-2 text-colorsecondary' /></a>
-                                        <a href=""><BiLinkExternal className='text-2xl m-2 text-colorsecondary' /></a>
-                                    </div>
-                                </div>
-                                <h2 className='py-2 font-bold hover:text-neon'>Integrating Algolia Search with WordPress Multisite</h2>
-                                <p className='text-sm py-2 text-colorsecondary'>Building a custom multisite compatible WordPress plugin to build global search with Algolia
-                                </p>
-                                <p className='py-2 text-sm text-colorsecondary'><span>Algolia</span><span className='p-2'>Wordpress</span><span>PHP</span></p>
-                            </div> */}
                         </div>
                     </div>
 
